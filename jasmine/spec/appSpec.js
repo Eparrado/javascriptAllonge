d9e02d604613abefc0584be8e415d162faf44110
Estela <estela.parrado@gmail.com>
2018-04-23 12:02:57 +0200

    functions

--- jasmine/spec/appSpec.js
+++ jasmine/spec/appSpec.js
@@ -2,12 +2,26 @@ describe("Javascript Allonge", function(){
     
     describe("References types", function(){
         it("Compares arrays", function(){
-            expect([2-1, 2, 2+1] === [1,2,3]).toEqual(false); 
+            expect([2-1, 2, 2+1] === [1,2,3]).toEqual(false); //Es false porque trato de comparar referencias diferentes
             expect([1,2,3] === [1, 2, 3]).toEqual(false);
             expect([1, 2, 3] === [1, 2, 3]).toEqual(false);
-            expect([1, 2, 3]).toEqual([1, 2, 3]);
+            expect([1, 2, 3]).toEqual([1, 2, 3]); //son iguales porque su contenido es el mismo
             expect([2-1, 2, 2+1]).toEqual([1, 2, 3]);
         });
-    }) 
 
-})
+        it("Compares functions", function() {
+            expect((function(){}) === (function(){})).toEqual(false);
+        })
+
+        it("Undefined expressions", function() {
+            expect((function(){})() === (function(){})()).toEqual(true); //Estoy comparando undefined con undefined porque éste es un valor y como tal puede ser comparado.
+            expect(undefined === undefined).toEqual(true); //Undefined: the absences of value, but also its own type of value. Every undefined is identical to every other undefined != null.
+            expect(void 0 === undefined).toEqual(true);
+            expect(void (2 + 2) === undefined).toEqual(true);
+            //Qué necesidad tengo de generar un undefined? Caso de uso para void.
+        })
+
+        it("")
+    })  
+
+}) 
--- script/main.js
+++ script/main.js
@@ -1,3 +1,24 @@
 /*references types*/
 var arr0 = [2-1, 2, 2+1];
 var arr1 = [1, 1+1, 1+1+1];
+
+/*Basic function 
+
+function (x) {               Esta x es argumento
+    return (function (y) {   Esta y es argumento 
+        return x             Esta x es una expresión referida a una variable. Free variable porque no está definida por los parámetros de esta función. Not bind to this function (not bound). 
+    });
+};
+
+Cada vez que invoco una función (le paso valores a los parámetros) se crea un nuevo ambiente en el que las valiarbles son igualadas a valores (a los argumentos que le pasamos).
+Como si fuera una nueva entrada al diccionario en el que igualo el parámetro a su valor en esa invocación.
+*/
+
+/*Pure functions vs Closures
+1. Functions containing no free variables.
+2. Functions containing one or more free variables.
+
+El ejemplo anterior podría parecer un closure pero no lo es.
+La function(y) es una closure pero está contenida dentro de function(x), la cual tiene un a variable bound así que es una pure function.
+=> a pure function can contain a closure.
+*/
